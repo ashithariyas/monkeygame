a6d9d0e8a8ecb2d3aa1e31a1841f71da2a22b764
Ashitha <ashithariyas13@gmail.com>
2020-12-06 19:03:04 +0100

    test comit

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var FoodGroup, obstacleGroup
 var score
 
 function preload(){
-  
+  //test
   
   monkey_running =            loadAnimation("monkey_0.png","monkey_1.png","monkey_2.png","monkey_3.png","monkey_4.png","monkey_5.png","monkey_6.png","monkey_7.png","monkey_8.png")
   
